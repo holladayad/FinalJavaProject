f8137e940223c37a729144fd623ac8e02152c7ec
holladayad <amberholladay21@gmail.com>
2020-04-21 23:32:14 -0400

    Implemented Course
    finished course up and added a few things to student that course needed

--- Course.java
+++ Course.java
@@ -1,4 +1,8 @@
+// Created By Amber Holladay and Gonzo Ocampo
+// Course Class
+
 package FinalProject.FinalJavaProject;
+import java.util.*;
 
 public class Course 
 {
@@ -8,6 +12,8 @@ public class Course
    private int courseCapacity; 
    private int CourseID;
    private static int nextCourseID = 0;
+   private ArrayList<Student> enrolledStudents;
+   private Instructor courseInstructor;
       
     // constructor 
     public Course(String name, String building, String room, int capacity)
@@ -17,26 +23,38 @@ public class Course
         this.courseBuilding = building;
         this.courseBldgRoom = room;
         this.courseCapacity = capacity;
+        this.enrolledStudents = new ArrayList<>();
+        this.courseInstructor = null;
         
+        
+        // I don't think this will work well
         //  this needs to be added into main:   (So im writing the class assuming its already in there 
         // public static ArrayList<Student> studentList = new ArrayList<Student>();
-        Application.studentList.add(this);
+        // Application.studentList.add(this);
     }
     
    public void enrollStudent(Student newStudent)
    {
-      
+      this.enrolledStudents.add(newStudent);
    }
    
    public boolean removeStudent(int studentID)
    {
-       
-       return true;
+       for (int i=0; i<enrolledStudents.size();i++)
+       {
+           Student currentStudent = this.enrolledStudents.get(i);
+           if (currentStudent.getStudentID() == studentID)
+           {
+               this.enrolledStudents.remove(i);
+               return true;
+           }
+       }
+       return false;
    }
    
    public void assignInstructor(Instructor newInstructor)
    {
-      
+      this.courseInstructor = newInstructor;
    }
   
    public String setCourseBldgroom(String room)
@@ -60,14 +78,37 @@ public class Course
        this.courseName = name;
    }
    
+   @Override
    public String toString()
    {
-       String toString = "";
+       String instructor = "None";
+       if (this.courseInstructor!=null)
+       {
+           instructor = this.courseInstructor.toString();
+       }
+       String toString = String.format("Course #%d Course: %s Location: %s Room: %s Capacity: %d\n"
+               + "Instructor of Record: %s", this.CourseID, this.courseName, this.courseBuilding,
+               this.courseBldgRoom, this.courseCapacity, instructor);
        return toString;
    }
    
    public String getRoster()
    {
-       return this.roster;
+       if(this.enrolledStudents.isEmpty())
+       {
+           return "This course has no students enrolled.";
+       }
+       
+       String roster = "";
+       
+       for(int i=0;i<this.enrolledStudents.size();i++)
+       {
+          Student current = this.enrolledStudents.get(i);
+          String s = String.format("Student ID#: %d %s Major: %s Year: %s\n", 
+                  current.getStudentID(), current.getName(), current.getStudentMajor(), 
+                  current.getStudentYear());
+          roster += s;
+       }
+       return roster;
    }
 }
--- Student.java
+++ Student.java
@@ -2,5 +2,40 @@ package FinalProject.FinalJavaProject;
 
 
 public class Student {
+    private String firstName;
+    private String lastName;
+    private String studentYear;
+    private String studentMajor;
+    private double GPA;
+    private String studentEmail;
+    private int studentID;
+    private static int nextStudentID = 1000;
     
+    
+    
+    
+    
+    
+    
+    public String getName()
+    {
+        return this.lastName + ", " + this.firstName;
+    }
+    
+    
+    public int getStudentID()
+    {
+        
+        return this.studentID;
+    }
+    
+    public String getStudentYear()
+    {
+        return this.studentYear;
+    }
+    
+    public String getStudentMajor()
+    {
+        return this.studentMajor;
+    }
 }
