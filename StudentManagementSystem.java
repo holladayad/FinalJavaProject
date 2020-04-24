272a83b47670dd2daf2cdf45ba2eb6f47ab29e60
gonzoocampo <64029613+gonzoocampo@users.noreply.github.com>
2020-04-24 13:07:48 -0400

    Revert "Merge branch 'master' of https://github.com/holladayad/FinalJavaProject"
    This reverts commit d24f3c62b838ee5a1a9385689aa78d110d2855ff, reversing
    changes made to e8166d6557ba0e34da1b5f4e539eef6a03ad1fed.

---
+++ StudentManagementSystem.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FinalProject.FinalJavaProject;
+
+/**
+ *
+ * @author Amber
+ */
+public class StudentManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        public static ArrayList<Student> studentArray = new ArrayList<>();
+        public static ArrayList<Course> courseArray = new ArrayList<>();
+        public static ArrayList<Instructor> instructorArray = new ArrayList<>();
+
+    }
+    
+}
