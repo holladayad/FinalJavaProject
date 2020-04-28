ad7feeaf55f29341c2b8b6a3d69ea0ce84ddb2a1
kowahlpm <kowahlpm@dukes.jmu.edu>
2020-04-28 13:36:13 -0400

    Create StudentFXSystem.java

---
+++ StudentFXSystem.java
@@ -0,0 +1,46 @@
+/*
+Authors:
+Date: Apr 28, 2020
+Assignment: Group Project Part 2
+Purpose: JavaFX Version of the Student Management System
+ */
+package FinalProject.FinalJavaProject;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class StudentFXSystem extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
